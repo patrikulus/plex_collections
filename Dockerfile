7577d237d809b66b84ec7442d22b12c9709f695c
Patryk Lotzwi <plotzwi@objectivity.co.uk>
2021-05-03 14:46:23 +0200

    Added missing default config variables

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM rackspacedot/python37:latest
 
 ENV CUSTOM_POSTER_FILENAME=movieset-poster-custom
 ENV LOCAL_POSTER_FILENAME=movieset-poster
+ENV CUSTOM_ART_FILENAME=movieset-background-custom
+ENV LOCAL_ART_FILENAME=movieset-background
 ENV PLEX_URL=http://localhost:32400
 ENV PLEX_TOKEN=""
 ENV SCAN_INTERVAL=300
--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 echo "" > config.yaml
 echo "custom_poster_filename: $CUSTOM_POSTER_FILENAME" >> config.yaml
 echo "local_poster_filename: $LOCAL_POSTER_FILENAME" >> config.yaml
+echo "custom_art_filename: $CUSTOM_ART_FILENAME" >> config.yaml
+echo "local_art_filename: $LOCAL_ART_FILENAME" >> config.yaml
 echo "plex_token: $PLEX_TOKEN" >> config.yaml
 echo "plex_url: $PLEX_URL" >> config.yaml
 echo "tmdb_key: $TMDB_KEY" >> config.yaml
